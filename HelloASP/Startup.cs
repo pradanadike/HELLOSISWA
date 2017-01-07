5066accfa579f21bec1383b5c05f78d0e8c92d8a
pradanadike <pradanadike5@gmail.com>
2017-01-07 20:42:04 +0700

    Add project files.

---
+++ HelloASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloASP.Startup))]
+namespace HelloASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
